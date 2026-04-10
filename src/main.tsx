d8942049d15bdd0c54893cb5258a4a06c4d12c21
Amaan Siddiqui <147709926+mohdamaansiddiqui@users.noreply.github.com>
2026-04-10 21:11:25 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Add this
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* The basename must match your repo name exactly */}
+    <BrowserRouter basename="/portfolio-/">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
